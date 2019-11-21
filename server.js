cb1a9e26f615fa31fd90b2cc782db48bc52d5293
bevincass <peteraugros@gmail.com>
2019-11-20 21:38:15 -0700

    final again

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 const mainDir = path.join(__dirname, "/public");
 
 //middleware
